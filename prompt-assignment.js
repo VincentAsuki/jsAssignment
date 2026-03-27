c290851cdbe5a809392d44c94431b7496154b8c9
VincentAsuki <vincentasuki@gamil.com>
2026-03-27 02:11:50 +0100

    Testing

--- prompt-assignment.js
+++ prompt-assignment.js
@@ -1,23 +1,24 @@
-// Create an initials generator program
+// Create an initials generator program.
 var name = prompt("Enter First name");
 var surname = prompt("Enter Surname");
 
-var firstletter = name[0];
+
+var firstletter1 = name[0];
 var firstletter2 = surname[0];
 
-var initials = `${firstletter}.${firstletter2}`
+var initials = `${firstletter1}.${firstletter2}`
 
 console.log(initials)
 
 
-// Create a word position finder
+// Create a word position finder.
 var sentence = prompt("Enter a sentence");
 var word = prompt("Enter a word to search for");
 
 var position = sentence.indexOf(word);
 console.log(position)
 
-// Build a username generator
+// Build a username generator.
 var firstname = prompt("Enter First name");
 var surname = prompt("Enter Surname");
 
